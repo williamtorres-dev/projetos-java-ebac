2ec99f91603927d885b6d40b07841e2fac9089d4
williamtorres-dev <williammacairatorres@gmail.com>
2025-10-20 17:16:40 -0300

    inlcuido o exercício de verificar o maior número na pasta mod7

---
+++ mod6/conversao-de-string/src/ConversaoDeString.java
@@ -0,0 +1,15 @@
+public class ConversaoDeString {
+
+    public static void main(String[] args) {
+
+        String numero = "5";
+
+        int numeroConvertido = Integer.parseInt(numero);
+
+        int valorSoma = numeroConvertido + 10;
+
+        System.out.println(valorSoma);
+
+    }
+
+}
---
+++ mod7/maior-numero/src/MaiorNumero.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MaiorNumero {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" ");
+        System.out.println("Insira dois números inteiros.");
+        System.out.println("Primeiro Número: ");
+        int num1 = scanner.nextInt();
+        System.out.println(" ");
+        System.out.println("Segundo Número: ");
+        int num2 = scanner.nextInt();
+        System.out.println(" ");
+
+        if (num1 > num2) {
+            System.out.println("O maior número é: " + num1);
+        } else if (num2 > num1) {
+            System.out.println("O maior número é: " + num2);
+        } else {
+            System.out.println("Os números são iguais!");
+        }
+    }
+
+}
